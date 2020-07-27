bf7c22d262648951d8de5596a8a9f1cb69a03853
erickLuizA <erickluiz69@gmail.com>
2020-07-27 09:10:53 -0300

    redirect change

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 3333
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'https://findove.netlify.app'
+}))
 
 app.use(routes)
 
